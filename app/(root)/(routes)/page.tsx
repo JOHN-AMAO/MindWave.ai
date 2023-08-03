5384e250fdd90e4e9e9a6cca76b4b655121027ae
JOHN-AMAO <amaojohn767@gmail.com>
2023-08-03 15:14:09 +0100

    Implementation of autentication and more

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,11 @@
-const page = () => {
-  return <div>Root page</div>;
+import { UserButton } from "@clerk/nextjs";
+
+const Rootpage = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  );
 };
 
-export default page;
+export default Rootpage;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='h-full'>
+      <main className='md:pl-20 pt-16 h-full'>{children}</main>
+    </div>
+  );
+};
+
+export default RootLayout;
