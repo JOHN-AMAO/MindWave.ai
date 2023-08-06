0281855b42232882fc368064c6bc79769b6d2458
JOHN-AMAO <amaojohn767@gmail.com>
2023-08-06 03:47:08 +0100

    "Dark mode added"

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,21 +1,53 @@
 "use client";
 
+import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
-import { Menu } from "lucide-react";
+import { Poppins } from "next/font/google";
+import { Sparkles } from "lucide-react";
 
-const Navbar = () => {
+import { cn } from "@/lib/utils";
+import { MobileSidebar } from "@/components/mobile-sidebar";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { useProModal } from "@/hooks/use-pro-modal";
+
+const font = Poppins({ weight: "600", subsets: ["latin"] });
+interface NavbarProps {
+  isPro: boolean;
+}
+
+export const Navbar = ({ isPro }: NavbarProps) => {
+  const proModal = useProModal();
+
   return (
-    <div
-      className='fixed w-full flex justify-between
-  items-center py-2 px-4 border-b border-primary/10 bg-secondary'
-    >
+    <div className='fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary'>
       <div className='flex items-center'>
-        <Menu className='block md:hidden' />
+        <MobileSidebar isPro={isPro} />
+        <Link href='/'>
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
+            MindWave.ai
+          </h1>
+        </Link>
       </div>
-
-      <UserButton />
+      <div className='flex items-center gap-x-3'>
+        {!isPro && (
+          <Button
+            onClick={proModal.onOpen}
+            size='sm'
+            variant='premium'
+          >
+            Upgrade
+            <Sparkles className='h-4 w-4 fill-white text-white ml-2' />
+          </Button>
+        )}
+        <ModeToggle />
+        <UserButton afterSignOutUrl='/' />
+      </div>
     </div>
   );
 };
-
-export default Navbar;
